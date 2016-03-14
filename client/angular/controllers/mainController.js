1aed22ca6a6043d3e4ae3fc542954237d4ff25d3
Necholas Gomez <necholas.gomez@gmail.com>
2016-03-14 13:34:35 -0700

    no changes

--- client/angular/controllers/mainController.js
+++ client/angular/controllers/mainController.js
@@ -13,7 +13,6 @@ myApp.controller('mainController', function($scope, $window, auth, postFactory,$
     console.log("getting");
 
     postFactory.allPosts(function(data){
-    // console.log(data);
     posts = data;
     for (i in posts) {
       switch(posts[i].status){
@@ -31,44 +30,15 @@ myApp.controller('mainController', function($scope, $window, auth, postFactory,$
           break;
       }
     };
-    console.log($scope.models);
     });
   }
 $scope.get();
-// console.log("times");
-
-  // $scope.$watch('package', function() {
-  //   console.log('HERERHERHEREHRER');
-  //   $scope.models.lists.Completed.push(package);
-  //   console.log($scope.models.lists.Completed);
-  // });
-  // $scope.delivery = function(){
-  //   console.log(package);
-  //   $scope.posts.push(package);
-  //   $scope.get();
-  //   console.log("after get");
-  // }
-  // var package;
-
-  // $scope.listMoved = function(data){
-  //   console.log(data);
-  // }
-
-  $scope.newCardLocation = function(index, item, listName){
-
-    item.newLocation = listName;
-    item.newIndex = index;
-    postFactory.editPost(item, function(){
-
-    })
-  }
 
 
-  $scope.test = function (){
-    console.log("got here");
-    $scope.models.lists.Completed.push({'job_title':'test','company_name':'testtt'});
+  $scope.test = function(){
     console.log($scope.models.lists);
   }
+
 
     $scope.open = function (size) {
 
@@ -80,7 +50,7 @@ $scope.get();
       resolve: {
       options: function () {
         return ["Prospective","Applied","Pending","Completed"];
-        }
+      }
       }
       });
 
@@ -97,17 +67,6 @@ $scope.get();
         if (data.status == "Pending") {
           $scope.models.lists.Pending.push(data);
         }
-        console.log(data);
-        // console.log("above");
-        // $scope.get();
-        // console.log($scope.models);
-        // console.log("below");
-
-        // $scope.test();
-        // package = data;
-        // $scope.delivery();
-        // console.log(package);
-        // console.log($scope);
         });
     };
 
