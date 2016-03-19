b967a459ea6babb61c819124b79316810627611d
Necholas Gomez <necholas.gomez@gmail.com>
2016-03-18 22:06:58 -0700

    added mongoDB

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
 
-mongoose.connect('mongodb://localhost/soc');
+mongoose.connect('mongodb://heroku_0fvw2hcd:3fenrq3jp6qqhmgaqsptq7ac1e@ds013738.mlab.com:13738/heroku_0fvw2hcd');
 
 var models_path = path.join(__dirname, './../models');
 
