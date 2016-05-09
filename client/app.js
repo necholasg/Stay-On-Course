5064dc78c8ca9e5a0db07cd8f00b2011477610a6
Necholas Gomez <necholas.gomez@gmail.com>
2016-05-09 12:09:02 -0700

    added a prefilled user with directive

--- client/app.js
+++ client/app.js
@@ -10,6 +10,21 @@ myApp.run(function(defaultErrorMessageResolver){
   }
 );
 
+myApp.directive('ngInitial', function() {
+  return {
+    restrict: 'A',
+    controller: [
+      '$scope', '$element', '$attrs', '$parse', function($scope, $element, $attrs, $parse) {
+        var getter, setter, val;
+        val = $attrs.ngInitial || $attrs.value;
+        getter = $parse($attrs.ngModel);
+        setter = getter.assign;
+        setter($scope, val);
+      }
+    ]
+  };
+});
+
 myApp.config(['ngToastProvider', function(ngToast) {
     ngToast.configure({
       verticalPosition: 'top',
